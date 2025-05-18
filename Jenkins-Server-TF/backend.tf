4328178bb3a93c24b7084feaf9669dcda739876d
malamiller <121070837+malamiller@users.noreply.github.com>
2025-05-18 14:20:52 -0400

    Update backend.tf .1

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "swiggy-app-109
     region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
+    key            = "EKS/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
